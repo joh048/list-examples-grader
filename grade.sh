049e1961df2b5c6658066561e3101650597c2946
joh048 <146862219+joh048@users.noreply.github.com>
2023-11-28 08:36:38 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -18,8 +18,8 @@ then
     cp -r lib/ grading-area
     cd grading-area
 
-    javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
-    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+    javac -cp $CPATH *.java
+    java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
     if [[ $? == 0 ]]
     then
         echo 'Test Passed'
